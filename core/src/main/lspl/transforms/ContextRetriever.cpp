3192764545132236549ce282d0756c1ae639757a
Alexey Noskov <alexey.noskov@gmail.com>
2009-12-07 12:59:15 +0300

    ContextRetriever

---
+++ core/src/main/lspl/transforms/ContextRetriever.cpp
@@ -0,0 +1,55 @@
+/*
+ * ContextRetriever.cpp
+ *
+ *  Created on: Dec 6, 2009
+ *      Author: alno
+ */
+
+#include "ContextRetriever.h"
+
+#include "../text/Match.h"
+#include "../text/Loop.h"
+#include "../patterns/Alternative.h"
+#include "../patterns/matchers/Matcher.h"
+#include "../patterns/matchers/MatcherContainer.h"
+#include "../patterns/matchers/LoopMatcher.h"
+
+using lspl::patterns::matchers::Context;
+using lspl::patterns::matchers::LoopMatcher;
+
+using lspl::text::Loop;
+using lspl::text::LoopIterationVariant;
+
+namespace lspl { namespace transforms {
+
+ContextRetriever::ContextRetriever() {
+}
+
+ContextRetriever::~ContextRetriever() {
+}
+
+Context ContextRetriever::apply( const text::MatchVariant & matchVariant ) const {
+	Context ctx;
+	appendToContext( ctx, matchVariant, matchVariant.alternative );
+	return ctx;
+}
+
+void ContextRetriever::appendToContext( patterns::matchers::Context & ctx, const text::Transition & transition, const patterns::matchers::Matcher & matcher ) const {
+	if ( const LoopMatcher * loopMatcher = dynamic_cast<const LoopMatcher *>( &matcher ) ) {
+		const Loop & loop = dynamic_cast<const Loop &>( transition );
+
+		for ( uint i = 0; i < loop.getIterationCount(); ++ i ) {
+			const LoopIterationVariant & v = loop.getIteration( i )->getVariant( 0 );
+			appendToContext( ctx, v, v.alternative );
+		}
+	} else {
+		ctx.addValue( matcher.variable, &transition );
+	}
+}
+
+void ContextRetriever::appendToContext( patterns::matchers::Context & ctx, const text::TransitionList & transitions, const patterns::matchers::MatcherContainer & matchers ) const {
+	for ( uint i = 0; i < transitions.size(); ++ i )
+		appendToContext( ctx, *transitions[i], matchers.getMatcher( i ) );
+}
+
+} } // namespace lspl::transforms
---
+++ core/src/main/lspl/transforms/ContextRetriever.h
@@ -0,0 +1,31 @@
+/*
+ * ContextRetriever.h
+ *
+ *  Created on: Dec 6, 2009
+ *      Author: alno
+ */
+
+#ifndef _LSPL_TRANSFORMS_CONTEXTRETRIEVER_H_
+#define _LSPL_TRANSFORMS_CONTEXTRETRIEVER_H_
+
+#include "Transform.h"
+#include "../patterns/matchers/Context.h"
+
+namespace lspl { namespace transforms {
+
+class LSPL_EXPORT ContextRetriever : public TypedTransform<patterns::matchers::Context> {
+public:
+	ContextRetriever();
+	virtual ~ContextRetriever();
+
+	virtual patterns::matchers::Context apply( const text::MatchVariant & matchVariant ) const;
+private:
+
+	void appendToContext( patterns::matchers::Context & ctx, const text::Transition & transition, const patterns::matchers::Matcher & matcher ) const;
+	void appendToContext( patterns::matchers::Context & ctx, const text::TransitionList & transitions, const patterns::matchers::MatcherContainer & matchers ) const;
+
+};
+
+} } // namespace lspl::transforms
+
+#endif /* _LSPL_TRANSFORMS_CONTEXTRETRIEVER_H_ */
