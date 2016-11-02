e9e27e6b0642095c407f314e742eb54bae47d292
Alexey Noskov <alexey.noskov@gmail.com>
2016-11-02 22:56:45 +0300

    Update JNI stub headers

--- bindings-java/src_cpp/lspl_jni/ru_lspl_patterns_Pattern_DependencyList.h
+++ bindings-java/src_cpp/lspl_jni/ru_lspl_patterns_Pattern_DependencyList.h
@@ -7,6 +7,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+#undef ru_lspl_patterns_Pattern_DependencyList_MAX_ARRAY_SIZE
+#define ru_lspl_patterns_Pattern_DependencyList_MAX_ARRAY_SIZE 2147483639L
 #ifdef __cplusplus
 }
 #endif
