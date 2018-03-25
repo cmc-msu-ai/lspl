559cb79d55d32ba78b243afe897632b2d5d068e1
alesapin <alesapin@gmail.com>
2018-03-25 15:41:53 +0300

    Fix bootstrap

--- deps/bootstrap.sh
+++ deps/bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-cd ./pcre && git submodule update && cmake . && make -j
-cd ..
+path=$(pwd)
+git submodule update --recursive pcre
+cd ./pcre && cmake . && make -j
+cd $path
 if [[ $(ldconfig -p | grep libboost) ]]; then
     echo "System boost found"
 else
     echo "System boost not found, will compile from srcs"
-    cd boost && git submodule update && ./b2 cxxflags=-fPIC cflags=-fPIC install --link=static --runtime-link=static --prefix=./build/
+    git submodule update --recursive boost
+    cd boost && ./b2 cxxflags=-fPIC cflags=-fPIC install --link=static --runtime-link=static --prefix=./build/
 fi
-cd ..
-
+cd $path
+cd aot && ./build-linux.sh
 
